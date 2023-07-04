36a2f57500a742db0e19ee55c3c97c036424e3cd
nando-2410 <escorpio2003luisf@gmail.com>
2023-07-04 10:43:52 -0500

    push

--- mate.js
+++ mate.js
@@ -1,1 +1,19 @@
-alert("Hola")
+//alert("Hola");
+/* comentario de varias lines */
+var codigo;
+let nombre;
+
+const dato="2023";
+var edad;
+edad= 20;
+if(edad > 18){
+    alerta("Mayor de edad");
+}
+else{
+    alert("nooo");
+}
+
+var contador 
+for(contador=0; contador < 20;contador++){
+console
+}
